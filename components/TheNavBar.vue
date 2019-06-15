10901c69453009c0fac44ea8a5016facc3dbae09
Jordan <knightworks@pm.me>
2019-06-15 13:14:36 -0500

    initial commit

---
+++ components/TheNavBar.vue
@@ -0,0 +1,25 @@
+<template id="nav-bar">
+    <div>
+      <nuxt-link
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          :key="post.id"
+          class="button--grey"
+        >
+          {{post.title}}
+        </nuxt-link>
+    </div>
+
+</template>
+<script charset="utf-8">
+export default {
+  components: {
+  },
+  computed: {
+    posts () {
+      return this.$store.state.posts.all
+    }
+  }
+}
+</script>
+</script>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">{{post.title}}</h1>
+      <p>{{post.content}}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        id: this.$route.params.id,
+      }
+    },
+    computed: {
+      post () {
+        return this.$store.state.posts.all.find(post => post.id === this.id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
